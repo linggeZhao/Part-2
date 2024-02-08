90f1f8d5d651dbe5877cbab3e54cd33d165489ef
zhalingg <zhalingg@OA-S141-18>
2024-02-08 14:24:51 -0500

    Task1 complete: player make damage.

--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -9,6 +9,7 @@ public class Knight : MonoBehaviour
     public float speed = 3;
     Rigidbody2D rb;
     Animator animator;
+    bool clickingOnself = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,10 +32,20 @@ public class Knight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) && !clickingOnself)
         {
             destination = Camera.main.ScreenToWorldPoint (Input.mousePosition);
         }
         animator.SetFloat("Movement", movement.magnitude);
+    }
+
+    private void OnMouseDown()
+    {
+        clickingOnself  = true;
+        animator.SetTrigger("TakeDamage");
+    }
+    private void OnMouseUp()
+    {
+        clickingOnself = false;
     }
 }
