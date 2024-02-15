274bc7a5c837d517082f4cdb952bf400cf829ae3
zhalingg <zhalingg@OA-S141-25>
2024-02-15 15:20:20 -0500

    Task2 complete: Make more scenes.

---
+++ Assets/Week 6/Scripts/SceneName.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SceneName : MonoBehaviour
+{
+    TextMeshProUGUI sceneNameLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneNameLabel = GetComponent<TextMeshProUGUI>();
+        sceneNameLabel.text = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
