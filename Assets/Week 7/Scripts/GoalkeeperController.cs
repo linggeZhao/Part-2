38c713fe1639e2c5eab80ebf2e2af23d3426d3e6
zhalingg <zhalingg@OA-S141-24>
2024-02-22 18:33:09 -0500

    Task4a done: make a goalkeeper.

---
+++ Assets/Week 7/Scripts/GoalkeeperController.cs
@@ -0,0 +1,29 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalkeeperController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 5;
+    Vector2 position;
+    public float maxDistance = 3;
+
+
+    private void FixedUpdate()
+    {
+        if (Controller.SelectedPlayer == null)
+        {
+            return;
+        }
+        position = ((Vector2)transform.position + (Vector2)Controller.SelectedPlayer.transform.position)/2;
+        Vector2 distance = (Vector2)transform.position - position;
+        if (distance.magnitude > maxDistance)
+        {
+            position = (Vector2)transform.position + maxDistance * -distance.normalized;
+        }
+        rb.MovePosition(position);
+    }
+}
